6f855b87b1a8f6077f0aa6ca780c4e42de9f13d9
Vimal <vimworks.ai@gmail.com>
2026-07-29 19:24:24 +0530

    Structures

--- c-cpp/week0-bridge/arrays_basics.cpp
+++ c-cpp/week0-bridge/arrays_basics.cpp
@@ -6,15 +6,32 @@ using namespace std; // allows you to use identifiers from the Standard Library 
 
 int main(){
 
-    int A[10];
-    
-    cout << sizeof(A[1])<<endl;
-    cout << sizeof(A)<<endl;
-    printf("%d",A[4]);
-    
-    
+    int A[10]  = {0};
+    // Normal For loop:
+    //for (int i = 0; i<10; i++){
+    //   cout << A[i] << endl;
+    //}
 
+    // Easy way to loop in Cpp:
+    // ranged-based for loop (or range-for loop) in C++
 
+    //for (int x:A){
+    //    cout << x << endl;
+    //}
+
+    // We can also get make some array of variable size
+    int n;
+    cout<< "Enter the Size"<< endl;
+    cin>>n;
+    
+    // int B[n] = {1,2,3}; // We can't initialize variable sized array , 
+    // you can uncomment it and see.
+    int B[n]; // this will work.
+
+    for (int x:B){
+        cout << x << endl;
+    }
+    
 
     return 0;
 }
