20845adfb271fdba116960aab80efbe9ab384bcf
Vimal <vimworks.ai@gmail.com>
2026-08-13 23:41:10 +0530

    Aug 13

---
+++ c-cpp/week0-bridge/rectangleOOP.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Rectangle
+{
+    int length;
+    int breadth;
+
+public:
+
+    Rectangle(){
+        length = 0;
+        breadth = 0;
+    }
+    Rectangle(int l, int b){
+        length = l;
+        breadth = b;
+    }
+    
+    
+};
+int main(){
+
+
+    return 0;
+}
