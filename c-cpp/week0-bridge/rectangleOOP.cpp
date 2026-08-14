c26ffb04c51cb44dd22bbe8e21722b1b45b59f38
Vimal <vimworks.ai@gmail.com>
2026-08-14 19:01:06 +0530

    Done a simple rectangle program using OOP

--- c-cpp/week0-bridge/rectangleOOP.cpp
+++ c-cpp/week0-bridge/rectangleOOP.cpp
@@ -4,24 +4,60 @@ using namespace std;
 
 class Rectangle
 {
+private:
     int length;
     int breadth;
 
 public:
 
-    Rectangle(){
+    Rectangle(){  // Default constructor
         length = 0;
         breadth = 0;
     }
-    Rectangle(int l, int b){
+    Rectangle(int l, int b)
+    { // Parameterized constructor
         length = l;
         breadth = b;
     }
     
+    int area()
+    {
+        return length*breadth;
+    }
+    int perimeter()
+    {
+        return 2*(length+breadth);
+    }
+    
+    void setLength(int l)
+    {
+        length = l;
+    }
+    
+    void setBreadth(int b)
+    {
+        breadth = b;
+    }
+    
+    int getLength()
+    {
+        return length;
+    }
+    
+    int getBreadth()
+    {
+        return breadth;
+    }
+    ~Rectangle(){
+    cout<<"This will distruct if memory allocated"<<endl;
+}
     
 };
 int main(){
+    Rectangle r(10,5);
 
+    cout<<"Area "<<r.area()<<endl;
+    cout<<"Perimeter "<<r.perimeter()<<endl;
 
     return 0;
 }
