e529c522bd3e83fc03cb96eba8a085e3fec932e8
Vimal <vimworks.ai@gmail.com>
2026-08-24 23:13:01 +0530

    NCR

---
+++ c-cpp/week1-recursion/NCRsimple.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fact(int n)
+{
+    if (n==0) return 1;
+    return fact(n-1)*n;
+}
+
+int nCr(int n, int r)
+{
+    int num,den;
+
+    num = fact(n);
+    den = fact(r)*fact(n-r); 
+
+    return num/den;
+    
+    return 0;
+}
+
+int main()
+{
+    printf("%d",nCr(4,2));
+    return 0;
+}
